3d17cd2694ec438fd72263fb6cd0d59bce37574b
SuperMai <werhero@sina.cn>
2017-03-27 16:25:38 +0800

    Initial commit第一次

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Mr.Admin on 2017/3/27 0027.
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    // 重载父类
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        req.setAttribute("currentTime", currentTime);
+        req.getRequestDispatcher("/JSP/hello.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/JSP/hello.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Mr.Admin
+  Date: 2017/3/27 0027
+  Time: 15:34
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+<h1>Hello</h1>
+<h2>当前时间：${currentTime}</h2>
+<h3>Test</h3>
+</body>
+</html>
